05bc25b829341715b508c406912e20ec0fd441f8
CesarJr10 <juliancontre30@gmail.com>
2024-12-26 13:57:27 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchitectureExample.Application.Services;
+using CleanArchitectureExample.Core.Entities;
+using CleanArchitectureExample.Infrastructure.Repositories;
+using System;
+
+namespace CleanArchitectureExample.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productRepository = new InMemoryProductRepository();
+            var productService = new ProductService(productRepository);
+
+            var product = new Product
+            {
+                Id = 1,
+                Name = "pera",
+                Price = 1m
+            };
+
+            productService.CreateProduct(product);
+
+            var products = productService.GetAllProducts();
+
+            Console.WriteLine("Products in the repository:");
+            foreach (var p in products)
+            {
+                Console.WriteLine($"Id: {p.Id}, Name: {p.Name}, Price: {p.Price}");
+            }
+        }
+    }
+}
